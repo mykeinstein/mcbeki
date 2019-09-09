063e89af42d3fb1e5efe2b7ed72316144a7d49b4
mykeinstein <32529731+mykeinstein@users.noreply.github.com>
2019-09-09 01:24:17 -1100

    firstupdate

---
+++ backend/js/modal.js
@@ -0,0 +1,43 @@
+  'use strict';
+$(document).ready(function () {
+//Basic alert
+	document.querySelector('.sweet-1').onclick = function(){
+		swal("Here's a message!", "It's pretty, isn't it?")
+	};
+	//success message
+	document.querySelector('.alert-success-msg').onclick = function(){
+		swal("Good job!", "You clicked the button!", "success");
+	};
+
+	//Alert confirm
+	document.querySelector('.alert-confirm').onclick = function(){
+		swal({
+					title: "Are you sure?",
+					text: "Your will not be able to recover this imaginary file!",
+					type: "warning",
+					showCancelButton: true,
+					confirmButtonClass: "btn-danger",
+					confirmButtonText: "Yes, delete it!",
+					closeOnConfirm: false
+				},
+				function(){
+					swal("Deleted!", "Your imaginary file has been deleted.", "success");
+				});
+	};
+
+
+		$('#openBtn').on('click',function () {
+			$('#myModal').modal({
+				show: true
+			})
+		});
+
+		$(document).on('show.bs.modal', '.modal', function (event) {
+			var zIndex = 1040 + (10 * $('.modal:visible').length);
+			$(this).css('z-index', zIndex);
+			setTimeout(function() {
+				$('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+			}, 0);
+		});
+	});
+  
---
+++ backend/js/wizard.js
@@ -0,0 +1,19 @@
+  'use strict';
+  $(document).ready(function() {
+
+      // $('#date,#datejoin').bootstrapMaterialDatePicker({
+      //        time: false,
+      //        clearButton: true
+      //    });
+      //  $("#example-date-inputS").bootstrapMaterialDatePicker({
+      //                time: false,
+      //                clearButton: true
+      //            });
+
+      $("#basic-forms").steps({
+          headerTag: "h3",
+          bodyTag: "fieldset",
+          transitionEffect: "slideLeft",
+          autoFocus: true
+      });
+  });
